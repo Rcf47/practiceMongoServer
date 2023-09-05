bd574fc8dfb6b7a67813d75ee381b0dc81f61bfe
Vadim Cherepanov <xenos26@gmail.com>
2023-09-05 17:10:34 +0700

    feat: add routes on server

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const questRouter = require('./routes/quests-routes.js')
 
 const PORT = 3000;
 const URL = 'mongodb://localhost:27017/questbox'
 
 const app = express();
+
 app.use(express.json())
+app.use(questRouter)
 
 mongoose
   .connect(URL)
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => { console.log('Connected to MongoDB') })
   .catch((err) => { `DB connection error: ${err}` })
 
 app.listen(PORT, (err) => {
-  err ? console.err(err) : console.log(`listening ${PORT}`)
-});
+  err ? console.error(err) : console.log(`listening port ${PORT}`)
+})
